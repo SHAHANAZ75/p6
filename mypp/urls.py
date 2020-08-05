d99a96e88bd8dd5f83f2b72dcdb09f6624c63c59
unknown <shahanazshaik504@gmail.com>
2020-08-05 20:27:05 +0530

    third commit

--- mypp/urls.py
+++ mypp/urls.py
@@ -3,6 +3,6 @@ from mypp import views
 app_name="mypp"
 
 urlpatterns = [
-    path('trail/',views.trial,name="trial"),
+    path('trial/',views.trial,name="trial"),
     path('profile/',views.profile,name="profile"),
 ]
--- mypp/views.py
+++ mypp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def trial(request):
-    return HttpResponse("<h1> Project is on air")
+    return HttpResponse("<h1> Project is on air</h1>")
 def base(request):
     return render(request,"base.html")
 def home(request):
